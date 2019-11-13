93aba6b943bc76709639ef4068ac6479cd085ade
unknown <k.pempera@gmail.com>
2019-11-13 18:28:44 +0100

    Fixed bug with css not applying to all templates

--- uploads/urls.py
+++ uploads/urls.py
@@ -7,6 +7,6 @@ from uploads.core import views
 urlpatterns = [
     url('^$', views.home, name='home'),
     url('order_pizza/', views.order_pizza, name='order_pizza'),
-    url('your_orders', views.your_orders, name='your_orders'),
+    url('your_orders/', views.your_orders, name='your_orders'),
     url('contact/', views.contact, name='contact'),
 ]
