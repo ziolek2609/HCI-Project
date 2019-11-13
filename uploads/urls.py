2afd6e42c3e3fb622131c3656dd73060d3834463
Grzegorz Ziółkowski <50325280+ziolek2609@users.noreply.github.com>
2019-11-13 16:08:40 +0100

    added navbar
    Navbar style works only on 2 sides... dont know why

---
+++ uploads/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+from uploads.core import views
+
+
+urlpatterns = [
+    url('^$', views.home, name='home'),
+    url('order_pizza/', views.order_pizza, name='order_pizza'),
+    url('your_orders', views.your_orders, name='your_orders'),
+    url('contact/', views.contact, name='contact'),
+]
